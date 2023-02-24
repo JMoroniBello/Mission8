287270c29acf2b40609b50ec68957b790def9110
Connor Elder <connorelder1@gmail.com>
2023-02-23 21:51:58 -0700

    controller that includes actions for all the different views that will be needed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,16 +26,75 @@ namespace Mission8.Controllers
             return View();
         }
 
-       
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //Get and post views for adding something to the database; where a user can submit things
+
+        [HttpGet]
+        public IActionResult AddView()
+        {
+            ViewBag.Categories = theContext.Categories.ToList();
+
+            return View("AddView");
+        }
+
+        [HttpPost]
+        public IActionResult AddView(TaskResponse tr)
+        {
+            //this first if statement is for when all the correct fields are filled out
+            if (ModelState.IsValid)
+            {
+                theContext.Add(tr);
+                theContext.SaveChanges();
+                return View("Confirmation", tr);
+            }
+            else
+            {
+                //if all different forms are not filled in, it returns this view and shoots out the error messages associated with each required field
+                ViewBag.Categories = theContext.Categories.ToList();
+
+                return View();
+            }
+        }
+        //View that allows us to see the table that shows the Quadrant table
 
         public IActionResult QuadrantView()
         {
             return View();
         }
-        public IActionResult Error()
+        
+        //Views 
+        public IActionResult Edit(int taskid)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.Categories = theContext.Categories.ToList();
+
+            //Creating an object of application response type
+            var application = theContext.Responses.Single(x => x.TaskId == taskid);
+
+            return View("AddView", application);
+        }
+        [HttpPost]
+        public IActionResult Edit(TaskResponse tare)
+        {
+            theContext.Update(tare);
+            theContext.SaveChanges();
+
+            return RedirectToAction("QuadrantView");
+        }
+
+        //Creating actions that can delete records through a get and post
+        [HttpGet]
+        public IActionResult Delete(int taskid)
+        {
+            var application = theContext.Responses.Single(x => x.TaskId == taskid);
+
+            return View(application);
+        }
+        [HttpPost]
+        public IActionResult Delete(TaskResponse taskr)
+        {
+            theContext.Responses.Remove(taskr);
+            theContext.SaveChanges();
+
+            return RedirectToAction("QuadrantView");
         }
     }
 }
